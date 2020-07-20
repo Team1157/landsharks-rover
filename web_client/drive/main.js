180c0d67fff1f03c99e2d212961327eb331580e5
Noah <noah@mollerstuen.com>
2020-07-20 16:41:10 -0600

    Copied over Mikael's client and moved scripts to a separate file

---
+++ web_client/drive/main.js
@@ -0,0 +1,50 @@
+let moveAmountSlider = document.getElementById("moveAmountSlider");
+let moveAmountNumber = document.getElementById("moveAmountNumber");
+moveAmountNumber.value = moveAmountSlider.value;
+
+moveAmountSlider.oninput = function() {
+moveAmountNumber.value = this.value;
+};
+
+moveAmountNumber.oninput = function() {
+moveAmountSlider.value = this.value;
+};
+
+let moveSpeedSlider = document.getElementById("moveSpeedSlider");
+let moveSpeedNumber = document.getElementById("moveSpeedNumber");
+moveSpeedNumber.value = moveSpeedSlider.value;
+
+moveSpeedSlider.oninput = function() {
+moveSpeedNumber.value = this.value;
+};
+
+moveSpeedNumber.oninput = function() {
+moveSpeedSlider.value = this.value;
+};
+
+let rotateAmountSlider = document.getElementById("rotateAmountSlider");
+let rotateAmountNumber = document.getElementById("rotateAmountNumber");
+rotateAmountNumber.value = rotateAmountSlider.value;
+
+rotateAmountSlider.oninput = function() {
+rotateAmountNumber.value = this.value;
+document.getElementById("compass").style.transform = "rotate(" + rotateAmountSlider.value + "deg)";
+};
+
+rotateAmountNumber.oninput = function() {
+rotateAmountSlider.value = this.value;
+};
+
+let rotateSpeedSlider = document.getElementById("rotateSpeedSlider");
+let rotateSpeedNumber = document.getElementById("rotateSpeedNumber");
+rotateSpeedNumber.value = rotateSpeedSlider.value;
+
+rotateSpeedSlider.oninput = function() {
+rotateSpeedNumber.value = this.value;
+};
+
+rotateSpeedNumber.oninput = function() {
+rotateSpeedSlider.value = this.value;
+};
+
+function estop() {alert("stop stop STOP!!!");}
