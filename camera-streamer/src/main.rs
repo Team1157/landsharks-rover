c692b001cf224de51e9870047a6271c5ecde479c
Gary B <gary@gary600.xyz>
2022-08-05 11:34:03 -0600

    Add args to cam streamer

--- camera-streamer/src/main.rs
+++ camera-streamer/src/main.rs
@@ -1,13 +1,28 @@
+use std::path::PathBuf;
 use tungstenite::{connect, Message, Error as WsError};
+use clap::Parser;
 
+#[derive(Parser, Debug)]
+#[clap()]
+struct Args {
+    #[clap(value_parser)]
+    device: PathBuf,
+
+    #[clap(short, long, value_parser)]
+    framerate: Option<u32>,
+
+    #[clap(short, long, value_parser, number_of_values=2)]
+    resolution: Option<Vec<u32>>
+}
 
 fn main() {
-    let mut cam = rscam::new("/dev/video0").expect("failed to get camera device");
-    // Using default format which should be mjpeg
+    let args: Args = Args::parse();
+
+    let mut cam = rscam::new(args.device.to_str().unwrap()).expect("failed to get camera device");
 
     cam.start(&rscam::Config {
-        interval: (0, 30),
-        resolution: (1280, 720),
+        interval: (1, args.framerate.unwrap_or(10)),
+        resolution: args.resolution.map(|v| (v[0], v[1])).unwrap_or((640, 480)),
         format: b"MJPG",
         ..Default::default()
     }).expect("failed to init camera");
@@ -15,16 +30,18 @@ fn main() {
     let mut n = 0u32;
 
     'conn_loop: loop { // Endlessly try to connect
-        let (mut sck, _response) = match connect("ws://localhost:11572") {
+        let (mut sck, _response) = match connect("ws://rover.team1157.org:11572") {
             Ok(x) => x,
-            Err(e) => {println!("failed to connect: {e}"); continue;}
+            Err(e) => { println!("failed to connect: {e}"); continue; }
         };
         println!("connected");
         loop { // Endlessly send frames
+            println!("getting frame");
             let frame = cam.capture().expect("failed to get frame");
-            print!("frame {n}\r");
+            print!("frame {}: {:?}\r", n, frame.format);
+            assert_eq!(frame.format, b"MJPG"); // panic if can't get mjpg
             n += 1;
-            match sck.write_message(Message::Binary(frame.into_vec())) {
+            match sck.write_message(Message::Binary(frame.to_vec())) {
                 Ok(_) => (),
                 Err(e) => match e {
                     WsError::AlreadyClosed | WsError::ConnectionClosed => {
