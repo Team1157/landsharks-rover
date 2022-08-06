deac880668d1c474e258b1e6ea49f7ee5c9b03ac
Gary B <gary@gary600.xyz>
2022-08-06 12:27:37 -0600

    cam server tweaks

--- camera-streamer/src/main.rs
+++ camera-streamer/src/main.rs
@@ -1,10 +1,8 @@
 use std::error::Error;
 use std::io::Cursor;
 use std::path::PathBuf;
-use std::sync::Once;
 use tungstenite::{connect, Message, Error as WsError};
 use clap::Parser;
-use rscam::Frame;
 use image::io::Reader as ImageReader;
 
 #[derive(Parser, Debug)]
@@ -19,10 +17,10 @@ struct Args {
     #[clap(short, long, value_parser, number_of_values=2)]
     input_resolution: Option<Vec<u32>>,
 
-    #[clap(short, long, value_parser, number_of_values=2)]
+    #[clap(short='r', long, value_parser, number_of_values=2)]
     output_resolution: Option<Vec<u32>>,
 
-    #[clap(short, long, value_parser)]
+    #[clap(short='q', long, value_parser)]
     output_quality: Option<u8>
 }
 
@@ -30,13 +28,16 @@ struct Args {
 fn main() {
     // Parse args
     let args: Args = Args::parse();
+    let input_res = args.input_resolution.map(|v| (v[0], v[1])).unwrap_or((640, 480));
+    let output_res = args.output_resolution.map(|v| (v[0], v[1])).unwrap_or((256, 144));
+    let output_qual = args.output_quality.unwrap_or(50);
 
     // get camera device, config and start
     let mut cam = rscam::new(args.device.to_str().unwrap()).expect("failed to get camera device");
 
     cam.start(&rscam::Config {
         interval: (1, args.framerate.unwrap_or(10)),
-        resolution: args.input_resolution.map(|v| (v[0], v[1])).unwrap_or((640, 480)),
+        resolution: input_res,
         format: b"MJPG",
         ..Default::default()
     }).expect("failed to init camera");
@@ -52,14 +53,14 @@ fn main() {
         loop { // Endlessly send frames
             println!("getting frame");
             let frame = cam.capture().expect("failed to get frame");
-            println!("frame {}: {}, size {}", n, std::str::from_utf8(&frame.format).unwrap(), v.len());
+            println!("frame {}: {}, size {}", n, std::str::from_utf8(&frame.format).unwrap(), frame.len());
             assert_eq!(frame.format, *b"MJPG"); // panic if can't get mjpg
             n += 1;
             // reencode frame
             let encoded_frame = match reencode_frame(
                 &frame,
-                args.output_resolution.map(|v| (v[0], v[1])).unwrap_or((256, 144)),
-                args.output_quality.unwrap_or(50)
+                output_res,
+                output_qual
             ) {
                 Ok(f) => f,
                 Err(e) => { println!("Failed to reencode frame: {}", e); continue }
@@ -78,12 +79,12 @@ fn main() {
     }
 }
 
-fn reencode_frame(frame: &Frame, size: (u32, u32), quality: u8) -> Result<Vec<u8>, Box<dyn Error>> {
+fn reencode_frame(frame: &[u8], size: (u32, u32), quality: u8) -> Result<Vec<u8>, Box<dyn Error>> {
     let mut out = Vec::<u8>::new();
     ImageReader::new(Cursor::new(frame))
         .with_guessed_format()?
         .decode()?
-        .resize(size.0, size.1, image::imageops::FilterType::Lanczos3)?
-        .write_to(&mut out, image::ImageOutputFormat::Jpeg(quality));
+        .resize(size.0, size.1, image::imageops::FilterType::Lanczos3)
+        .write_to(&mut Cursor::new(&mut out), image::ImageOutputFormat::Jpeg(quality))?;
     Ok(out)
 }
